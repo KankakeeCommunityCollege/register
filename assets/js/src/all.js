99e5592fe2481e4259fd7af63950a6403f6e44f5
wdzajicek <tig.welder4@gmail.com>
2022-05-13 16:21:46 -0500

    Uncomment domain check in JS.

--- assets/js/src/all.js
+++ assets/js/src/all.js
@@ -5,8 +5,8 @@ import loadScript from './loadScript';
 window.addEventListener('load', () => {
   Promise.resolve()
     .then(() => {
-      // if (window.location.hostname.search(/\.kcc\.edu/) == -1)
-      //   return;
+      if (window.location.hostname.search(/\.kcc\.edu/) == -1)
+        return;
 
       // Load the Clarus Corp script first
       return loadScript('https://tag.simpli.fi/sifitag/6089e310-26f9-0136-e8b0-06659b33d47c', 'no-referrer-when-downgrade', true)
